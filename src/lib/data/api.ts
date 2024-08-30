449307695b69ec4d28b2bbbbbba575fbcc89d88c
dom <iam@bapairaew.com>
2024-08-30 13:58:58 +0700

    add cors

--- src/lib/data/api.ts
+++ src/lib/data/api.ts
@@ -5,7 +5,25 @@ type Params = {
   slug: string;
 };
 
+const allowedHeaders: HeadersInit = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+function isAllowedOrigin(origin: string | null) {
+  if (!origin) return false;
+  const allowedDomain = "bapairaew.com";
+  const regex = new RegExp(`^https?://([a-zA-Z0-9-]+\\.)?${allowedDomain}$`);
+  return regex.test(origin);
+}
+
 export const get = async <T extends Params>(request: Request, data: T[]) => {
+  const origin = request.headers.get("Origin");
+  let headers: HeadersInit | undefined = undefined;
+  if (isAllowedOrigin(origin)) {
+    headers = allowedHeaders;
+  }
   const url = new URL(request.url);
   const params = new URLSearchParams(url.search);
   const slug = params.get("slug");
@@ -13,10 +31,10 @@ export const get = async <T extends Params>(request: Request, data: T[]) => {
   if (slug) {
     const match = data.find((p) => p.slug === slug);
     if (match) {
-      return NextResponse.json([match]);
+      return NextResponse.json([match], { headers });
     }
     return notFound();
   }
 
-  return NextResponse.json(data);
+  return NextResponse.json(data, { headers });
 };
